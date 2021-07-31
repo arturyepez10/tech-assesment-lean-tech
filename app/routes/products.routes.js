69c008273ce00456657516b31177632d694db2ae
arturyepez10 <arturyepez10@gmail.com>
2021-07-30 23:47:55 -0400

    declared the routes for products

---
+++ app/routes/products.routes.js
@@ -0,0 +1,24 @@
+// Here we declare the routes of products
+module.exports = app => {
+    const controllers = require("../controllers").products;
+
+    // Router to declare the new routes
+    const router = require("express").Router();
+
+    // Create a product
+    router.post("/", controllers.create);
+
+    // Retrieves all Tutorials
+    router.get("/", controllers.findAll);
+
+    // Retrieve a single product
+    router.get("/:id", controllers.find);
+
+    // Update a product
+    router.put("/:id", controllers.update);
+
+    // Deletes a product
+    router.delete("/:id", controllers.delete);
+
+    app.use('/products/', router);
+}
